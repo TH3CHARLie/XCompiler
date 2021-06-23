e585db7978db20e3b100f4c4057689f503f5f148
Xuanda Yang <th3charlie@gmail.com>
2021-06-23 17:05:18 +0800

    update

--- src/escape_analysis.jl
+++ src/escape_analysis.jl
@@ -1,3 +1,15 @@
+using Core: ReturnNode
+
 function escape_analysis(ir::IRCode, ci::CodeInfo)
+    # TODO: at this moment, let's assume the function only has a
+    # single giant BaiscBlock
+    # a mapping from stmt idx to a set of escape values (represented by idx) at this stmt
+    escapes = Dict{Int, Set{Int}}()
+    for (idx, inst) in Iterators.reverse(enumerate(ir.stmts))
+        stmt = inst[:inst]
+        if isa(stmt, ReturnNode)
+            nothing
+        end
+    end
     return ir
 end
